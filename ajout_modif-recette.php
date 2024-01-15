cd666557c873c140a205b4354879215b69971ea2
Ballesteros-Romain <romain.ballesteros@gmail.com>
2024-01-15 23:54:23 +0100

    live 9

--- ajout_modif-recette.php
+++ ajout_modif-recette.php
@@ -2,13 +2,48 @@
     require_once('templates/header.php');
     require_once('lib/recipe.php');
     require_once('lib/tools.php');
+    require_once('lib/category.php');
+
+    $errors = [];
+    $messages = [];
 
     if (isset($_POST['saveRecipe'])){
+
+        // if (isset($_FILES['file']['tmp_name']) && $_FILES['file']['tmp_name'] != '') {
+        //     $checkImage = getimagesize($_FILES['file']['tmp_name']);    
+        //     if($checkImage !== false){
+        //         move_uploaded_file($_FILES['file']['tmp_name'], );
+        //     } else {
+        //         $errors[] = 'Le fichier doit être une image';
+        //     }
+        // }
+
         $res = saveRecipe($pdo, $_POST['category'], $_POST['title'], $_POST['description'], $_POST['ingredients'], $_POST['instructions'], null);
+
+        if($res){
+            $messages[] = 'La recette à bien été enregistré';
+        } else {
+            $errors[] = 'La recette n\'a pas été sauvegardé'
+;        }
     }
 
+    $categories = getCategories($pdo)
     
     ?>
+<h1>Ajouter une recette</h1>
+
+<?php foreach ($messages as $message) { ?>
+<div class="alert alert-success m-auto text-center" style="width: 50%;">
+    <?= $message ?>
+</div>
+<?php }  ?>
+
+<?php foreach ($errors as $error) { ?>
+<div class="alert alert-danger m-auto text-center" style="width: 50%;">
+    <?= $error ?>
+</div>
+<?php }  ?>
+
 <div style="margin-left: 50px; width: 50%" class="mt-5 m-auto">
     <form method="POST" enctype="multipart/form-data">
         <div class="mb-3">
@@ -33,9 +68,9 @@
         <div class="mb-3">
             <label for="category" class="form-label">Categories</label>
             <select name="category" id="category" class="form-select">
-                <option value="1">Entrée</option>
-                <option value="2">Plat</option>
-                <option value="3">Dessert</option>
+                <?php foreach ($categories as $categorie) { ?>
+                <option value="<?= $categorie['id'] ?>"><?= $categorie['name'] ?></option>
+                <?php } ?>
             </select>
         </div>
         <div class="mb-3">
---
+++ lib/category.php
@@ -0,0 +1,7 @@
+<?php
+function getCategories(PDO $pdo, int $limit = null){
+            $sql = 'SELECT * FROM categories';
+            $query = $pdo->prepare($sql);
+            $query->execute();
+            return  $query->fetchAll();
+        }
