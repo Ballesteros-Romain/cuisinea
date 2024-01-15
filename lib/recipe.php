42f25e4988dcd1e2dd2fdbb19c624242123e5a91
Ballesteros-Romain <romain.ballesteros@gmail.com>
2024-01-15 12:51:56 +0100

    suite cours

--- lib/recipe.php
+++ lib/recipe.php
@@ -3,5 +3,12 @@
         ["titre" => "Mousse au chocolat", 'description' => 'lorem40', 'image' => 'assets/images/cigale-olive.jpg'],
         ["titre" => "salade de chèvre", 'description' => 'lorem40', 'image' => 'assets/images/cigale-olive.jpg'],
         ["titre" => "gratin dauphinois", 'description' => 'lorem40', 'image' => 'assets/images/cigale-olive.jpg'],
-    ]
+    ];
+
+        function getRecipesById(PDO $pdo, int $id){
+           $query = $pdo->prepare("SELECT * FROM recipes WHERE id = :id");
+            $query->bindParam(':id', $id, PDO::PARAM_INT);
+            $query->execute();
+            return $query->fetch();
+        }
     ?>
--- recette.php
+++ recette.php
@@ -1,23 +1,42 @@
 <?php 
     require_once('templates/header.php');
     require_once('lib/recipe.php');
+    require_once('lib/tools.php');
     $id = $_GET['id'];
     // var_dump($recipes[$id]);
+
+    $pdo = new PDO("mysql:dbname=cuisinea;host=localhost:8889;charset=utf8mb4", "root", "root");
+
+    $id = (int)$_GET['id'];
+    $recipe = getRecipesById($pdo, $id);
+
+    $ingredients = linesToArray($recipe['ingredients']);
+    $instructions = linesToArray($recipe['instructions']);
 ?>
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-    <h1 class="title-recipes">Recette de <?= $recipes[$id]['titre'] ?> </h1>
+    <h1 class="title-recipes">Recette de <?= $recipe['title'] ?> </h1>
 </div>
 
 <!-- Recipes -->
 <div class="py-5">
-    <h1 class="display-5 fw-bold"><?= $recipes[$id]['titre'] ?></h1>
     <div class="col-lg-6 mx-auto">
-        <p class="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
-            popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.</p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Je commande</button>
-            <button type="button" class="btn btn-outline-light btn-lg px-4">Secret</button>
+        <p class="fs-5 mb-4"><?= $recipe['description']?></p>
+
+        <div class="col-lg-6 ">
+            <h2 class="mb-4">Ingeredients</h2>
+            <ul class="list-group">
+                <?php foreach ($ingredients as $key => $ingeredient) {?>
+                <li class="list-group-item"><?= $ingeredient; ?></li>
+                <?php } ?>
+            </ul>
+        </div>
+        <div class="col-lg-6 ">
+            <h2 class="mb-4 mt-5">Instructions</h2>
+            <ol class="list-group">
+                <?php foreach ($instructions as $key => $instruction) {?>
+                <li class="list-group-item"><?= $instruction; ?></li>
+                <?php } ?>
+            </ol>
         </div>
     </div>
 </div>
